ff01a54ca95034e88782e097b103b7b0b69fbf49
gauravbhaskar080 <gauravbhaskar111@gmail.com>
2024-07-18 22:31:47 +0530

    Day-3

---
+++ MATHEMATICAL/168-Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string convertToTitle(int columnNumber){
+    string result = "";
+    while(columnNumber > 0){
+        columnNumber--;
+        result = char(columnNumber % 26 + 'A') + result;
+        columnNumber /= 26;
+    }
+    return result;
+}
+
+int main(){
+    int n = 701;
+    cout << convertToTitle(n) << endl;
+    return 0;
+}
